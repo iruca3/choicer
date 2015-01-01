d3f95e82c54a7ddd6aaa48fcb10e5ed45563c9aa
Makoto NAKAYA <nakaya@icity-inc.jp>
2015-01-01 17:33:34 +0900

    remove dotenv load

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Choicer
   class Application < Rails::Application
-    Dotenv.load Rails.root.join('.env')
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
