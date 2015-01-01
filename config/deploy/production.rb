abf003d2bdc6cf85cc3ecc0efef80eff2641419e
Makoto NAKAYA <nakaya@icity-inc.jp>
2015-01-01 15:29:32 +0900

    .env 設定を追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :rails_env, 'production'
 set :migration_role, 'db'
 set :deploy_to, '/var/www/app/choicer'
 
-server '192.168.12.58', user: 'dolphin', roles: %w{ web app db }
+server '192.168.12.58', user: 'deploy', roles: %w{ web app db }
